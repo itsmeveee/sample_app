416a66ed011d0f795abb2c41d3afab445a3c15bd
noon <blahitsmev@gmail.com>
2014-12-11 11:43:05 +0000

    working on erb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,16 +3,26 @@ require 'spec_helper'
 describe "Static pages" do 
 	describe "Home page" do
 
-		it "should have the content 'Sample App' " do
+		it "should have the h1 'Sample App' " do
 			visit '/static_pages/home'
-			page.should have_content('Sample App')
+			page.should have_selector('h1', :text => 'Sample App')
 		end
+it "shoudl have the title 'Home'" do
+	visit '/static_pages/home'
+	page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Home')
+
+end
+
 	end
 
 	describe "Help page" do
 		it "should have the content 'Help Page'" do
 			visit '/static_pages/help'
 			page.should have_content('Help Page')
+		end
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Help')
 		end
 	end
 
@@ -21,6 +31,11 @@ describe "Static pages" do
 		visit '/static_pages/about'
 		page.should have_content ('About Page')
 	end
+	it "should have the title 'About'" do
+		visit '/static_pages/about'
+		page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | About')
+	end
+
 end
  	
 end
